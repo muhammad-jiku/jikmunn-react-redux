504d87d87464fa1c359263cb14238efe809b38f3
muhammad-jiku <muhammadjiku364@gmail.com>
2022-07-31 12:13:01 +0600

    mapDispatchToProps is added for own props method

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyJersey, buyKit } from '../redux/actionCreator';
 
 const ItemContainer = (props) => {
   return (
     <div>
       <h2>Item: {props.item} </h2>
+      <button onClick={props.buyItem}>Buy Item </button>
     </div>
   );
 };
@@ -18,4 +20,17 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.jersey
+    ? () => dispatch(buyJersey())
+    : () => dispatch(buyKit());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(
+  // null, // it is added when you don't want to send props but wants to dispatch
+  mapStateToProps,
+  mapDispatchToProps
+)(ItemContainer);
